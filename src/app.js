217f22692bc99b32983ec5d1ea177e085a960974
prachayaOrr <prachaya.orr@gmail.com>
2022-11-21 17:26:33 +0700

    add middleware notFound error

--- src/app.js
+++ src/app.js
@@ -11,5 +11,9 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
 const port = process.env.PORT || 8000;
 app.listen(port, () => console.log(`\n\n\nserver running on port: ${port}`));
